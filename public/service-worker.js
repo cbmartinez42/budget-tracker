4aff707c6d239e1ac0345ad7b1d5c8ab47b6ff58
cbmartinez42 <cbmartinez42@gmail.com>
2021-06-12 23:11:55 -0500

    correcting last bits

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,6 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/index.js",
     "/db.js",
-    // "/dist/index.js",
-    // "/dist/db.js",
     "/styles.css",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
